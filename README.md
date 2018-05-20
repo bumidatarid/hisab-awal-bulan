56dc26e5e8633dfee81ff402d64f3a413e4ae897
Priyadi Iman Nurcahyo <priyadi@priyadi.net>
2018-05-21 04:45:10 +0700

    commit awal

---
+++ README.md
@@ -0,0 +1,6 @@
+Script Hisab Awal Bulan
+=======================
+
+Contoh script Python sederhana untuk melakukan kalkulasi hisab awal bulan. Membutuhkan pyephem (`pip install pyephem`).
+
+Script dibuat sesederhana mungkin untuk keperluan pendidikan. Untuk keperluan serius, masih perlu dikembangkan, misalnya dengan melakukan perhitungan untuk seluruh kota yang ada di Indonesia.
---
+++ hisab.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+'''
+Script untuk menghitung hisab awal bulan.
+
+Algoritma: jika ketika Matahari terbenam, bulan di atas
+horizon & kemarin di bawah horizon, maka terjadi bulan baru.
+Seharusnya sudah mengakomodasi algoritma Muhammadiyah.
+
+Script memperhitungkan situasi jika ada kemungkinan rukyat
+tidak berhasil mengamati hilal, yaitu jika ketinggian bulan
+di bawah 3° saat matahari terbenam.
+
+Script hanya menghitung untuk lokasi Jakarta. Untuk
+penentuan bulan baru, seharusnya dilakukan untuk lokasi
+lain di Indonesia.
+'''
+import ephem
+from datetime import datetime, timedelta
+
+sekarang = datetime(2000, 1, 1, 5, 0, 0)
+selesai = datetime(2100, 12, 1, 5, 0, 0)
+
+bulan = ephem.Moon()
+matahari = ephem.Sun()
+
+pengamat = ephem.Observer()
+pengamat.lon = '106.816667'
+pengamat.lat = '-6.2'
+pengamat.elevation = 8
+
+ketinggiankemarin = 0
+
+while sekarang <= selesai:
+    pengamat.date = sekarang
+    terbenam = pengamat.next_setting(matahari)
+    pengamat.date = terbenam
+    bulan.compute(pengamat)
+
+    ketinggian = float(bulan.alt) / 0.01745329252
+
+    if ketinggian > 0 and ketinggiankemarin <= 0:
+        if ketinggian > 3:
+            print("%s-%s-%s: Masuk bulan baru. Ketinggian bulan: %s°" % (sekarang.year, sekarang.month, sekarang.day, ketinggian))
+        else:
+            print("%s-%s-%s: Muhammadiyah masuk bulan baru, pemerintah tergantung rukyat. Ketinggian bulan: %s°" % (sekarang.year, sekarang.month, sekarang.day, ketinggian))
+
+    ketinggiankemarin = ketinggian
+    sekarang += timedelta(days = 1)
